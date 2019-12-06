d2b15f1a648c443bba43420bb0a43ed77c1eccbe
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-06 12:41:44 +0200

    Updates

---
+++ precache-manifest.10e6afb4220f13248bd3d43e7b0c4ddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6ce122103e24059387d19e9e70f1f9",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "55215b1e91601b58f4bc",
+    "url": "/react_tabs/static/css/main.8e3d518c.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "55215b1e91601b58f4bc",
+    "url": "/react_tabs/static/js/main.adc84a3b.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
